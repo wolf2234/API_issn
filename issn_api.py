abbc13ff9bbdcce2cfe2a803c93951da6ac8f479
wolf2234 <viktoral9599@gmail.com>
2023-08-25 12:49:22 +0300

    this commit upgrades the file README.md.

--- issn_api.py
+++ issn_api.py
@@ -1,22 +1,11 @@
-import os
 import requests
 from typing import Union
 from time import sleep
-from pathlib import Path
-
-
-def get_issns_from_file() -> str:
-    """Функція повертає список issn, які зберігаються в файлі issn.txt"""
-    file_name = "issn.txt"
-    base_dir = Path(__file__).resolve().parent.parent
-    path = os.path.join(base_dir, f"data\{file_name}")
-    with open(path, "r", encoding="utf-8") as file:
-        for issn in file:
-            yield issn.strip()
 
 
 def load_data_by_url(url: str, params: dict) -> dict:
-    """Ця функція завантажує дані з браузера по url адресу.
+    """Функція завантажує дані через мережу.
+       Для цього вона робить запит по url адресу.
 
     :param url: url адрес ресурсу
     :param params: параметри запиту
@@ -27,7 +16,6 @@ def load_data_by_url(url: str, params: dict) -> dict:
     request_timeout = 1000
     attemp = 0
     data = {}
-    response = ""
 
     while attemp < max_attemp_count:
         try:
@@ -75,24 +63,21 @@ def get_by_issn(issn: str) -> dict:
 
 
 def parse_issn_data(issn: str, data: dict) -> dict:
-    """Функція аналізує дані по issn.
+    """Функція розпізнає дані по issn.
 
     :param issn: ідентифікатор issn
     :param data: дані по issn
     :return: Словник з даними по issn
-
     """
 
     result = {
         "issn": issn,
         "data": {},
         "parse_errors": {},
-        "parse_warnings": {},
         "unparsed_keys": [],
         "unsuccessful": [],
-        "unparsed_b": [],
         "errors": [],
-        "statistic": {},
+        "stats": {},
     }
 
     graph = data.get("@graph")
@@ -117,60 +102,44 @@ def parse_issn_data(issn: str, data: dict) -> dict:
         if id_attr.startswith("http://id.loc.gov/vocabulary/countries"):
             country = parse_country(item)
             errors = get_list_messages(country[1])
-            warnings = get_list_messages(country[2])
             issn_data["Country"] = country[0]
             result["parse_errors"].update(errors)
-            result["parse_warnings"].update(warnings)
         elif id_attr.startswith("organization/ISSNCenter"):
             organization = parse_oraganization(item)
             errors = get_list_messages(organization[1])
-            warnings = get_list_messages(organization[2])
             issn_data["Organization"] = organization[0]
             result["parse_errors"].update(errors)
-            result["parse_warnings"].update(warnings)
         elif id_attr == f"resource/ISSN/{issn}#ReferencePublicationEvent":
             country_code = parse_country_code(item)
             errors = get_list_messages(country_code[1])
-            warnings = get_list_messages(country_code[2])
             issn_data["CountryCode"] = country_code[0]
             result["parse_errors"].update(errors)
-            result["parse_warnings"].update(warnings)
         elif id_attr == f"resource/ISSN/{issn}#ISSN":
             resource_issn = parse_issn(issn, item)
             errors = get_list_messages(resource_issn[1])
-            warnings = get_list_messages(resource_issn[2])
             issn_data["ISSN"] = resource_issn[0]
             result["parse_errors"].update(errors)
-            result["parse_warnings"].update(warnings)
         elif id_attr == f"resource/ISSN/{issn}#ISSN-L":
             resource_issnL = parse_issnL(item)
             errors = get_list_messages(resource_issnL[1])
-            warnings = get_list_messages(resource_issnL[2])
             issn_data["ISSN-L"] = resource_issnL[0]
             result["parse_errors"].update(errors)
-            result["parse_warnings"].update(warnings)
         elif id_attr == f"resource/ISSN/{issn}#KeyTitle":
             key_title = parse_key_title(item)
             errors = get_list_messages(key_title[1])
-            warnings = get_list_messages(key_title[2])
             issn_data["KeyTitle"] = key_title[0]
             result["parse_errors"].update(errors)
-            result["parse_warnings"].update(warnings)
         elif id_attr == f"resource/ISSN/{issn}#Record":
             record = parse_record(item)
             errors = get_list_messages(record[1])
-            warnings = get_list_messages(record[2])
             issn_data.update(record[0])
             result["parse_errors"].update(errors)
-            result["parse_warnings"].update(warnings)
         elif id_attr == f"resource/ISSN/{issn}":
             another_resource_data = parse_issn_resource(item)
             errors = get_list_messages(another_resource_data[1])
-            warnings = get_list_messages(another_resource_data[2])
             issn_data["ISSN_resource"] = issn
             issn_data["resource"] = another_resource_data[0]
             result["parse_errors"].update(errors)
-            result["parse_warnings"].update(warnings)
         elif id_attr.startswith("resource/ISSN-L/"):
             result["unparsed_keys"].append("resource/ISSN-L/{issn-L}")
             unparsed += 1
@@ -219,34 +188,28 @@ def parse_issn_data(issn: str, data: dict) -> dict:
             )
             unparsed += 1
         elif id_attr.startswith("_:b"):
-            result["unparsed_b"].append(id_attr)
-            unparsed += 1
+            result["unsuccessful"].append(id_attr)
+            unsuccess += 1
         else:
             result["unsuccessful"].append(id_attr)
             unsuccess += 1
 
     result["data"] = issn_data
 
-    statistic = {
+    stats = {
         "total_keys": counter,
         "success_keys": (counter - unsuccess),
         "unsuccess_keys": unsuccess,
         "unparsed_keys": unparsed,
     }
 
-    result["statistic"] = statistic
+    result["stats"] = stats
 
     return result
 
 
 def parse_issn_resource(item: dict) -> tuple:
     errors = {"parse_issn_resource": []}
-    warnings = {"parse_issn_resource": []}
-
-    if "@type" not in item:
-        warnings["parse_issn_resource"].append(
-            f"attribute '@type' doesnt exist in {item}"
-        )
 
     if "url" not in item:
         errors["parse_issn_resource"].append(f"attribute 'url' doesnt exist in {item}")
@@ -297,49 +260,33 @@ def parse_issn_resource(item: dict) -> tuple:
         "isFormatOf": item.get("isFormatOf"),
         "isPartOf": item.get("isPartOf"),
     }
-    return (resource, errors, warnings)
+    return (resource, errors)
 
 
 def parse_oraganization(item: dict) -> Union[tuple, None]:
     errors = {"parse_oraganization": []}
-    warnings = {"parse_oraganization": []}
-
-    if "@type" not in item:
-        warnings["parse_oraganization"].append(
-            f"attribute '@type' doesnt exist in {item}"
-        )
 
     if item.get("@type") != "http://schema.org/Organization":
         errors["parse_oraganization"].append(
             f"attribute '@type' != 'http://schema.org/Organization', {item}"
         )
-        return (None, errors, warnings)
+        return (None, errors)
 
     code = item.get("@id").split("#")[1]
-    return (code, errors, warnings)
+    return (code, errors)
 
 
 def parse_country(item: dict) -> Union[tuple, None]:
     errors = {"parse_country": []}
-    warnings = {"parse_country": []}
-
-    if "@type" not in item:
-        warnings["parse_country"].append(f"attribute '@type' doesnt exist in {item}")
 
     if "label" not in item:
         errors["parse_country"].append(f"attribute 'label' doesn't exist in {item}")
 
-    return (item.get("label"), errors, warnings)
+    return (item.get("label"), errors)
 
 
 def parse_country_code(item: dict) -> Union[tuple, None]:
     errors = {"parse_country_code": []}
-    warnings = {"parse_country_code": []}
-
-    if "@type" not in item:
-        warnings["parse_country_code"].append(
-            f"attribute '@type' doesnt exist in {item}"
-        )
 
     if item.get("@type") != "http://schema.org/PublicationEvent":
         errors["parse_country_code"].append(
@@ -350,11 +297,11 @@ def parse_country_code(item: dict) -> Union[tuple, None]:
         errors["parse_country_code"].append(
             f"attribute 'location' doesn't exist in {item}"
         )
-        return (None, errors, warnings)
+        return (None, errors)
 
     location = item.get("location").split("/")
     code = location[len(location) - 1]
-    return (code, errors, warnings)
+    return (code, errors)
 
 
 def parse_status(item: dict) -> Union[tuple, None]:
@@ -390,28 +337,19 @@ def parse_format(item: dict) -> Union[tuple, None]:
 
 def parse_key_title(item: dict) -> Union[dict, tuple]:
     errors = {"parse_key_title": []}
-    warnings = {"parse_key_title": []}
-
     data = parse_item(item)
-
-    if "@type" not in item:
-        warnings["parse_key_title"].append(f"attribute '@type' doesnt exist in {item}")
 
     if "value" not in item:
         errors["parse_key_title"].append(f"attribute 'value' doesn't exist, {item}")
 
-    return (data, errors, warnings)
+    return (data, errors)
 
 
 def parse_issn(issn: str, item: dict) -> Union[dict, tuple]:
     errors = {"parse_issn": []}
-    warnings = {"parse_issn": []}
 
     parsed_issn = item.get("value")
     data = parse_item(item)
-
-    if "@type" not in item:
-        warnings["parse_issn"].append(f"attribute '@type' doesnt exist in {item}")
 
     if item.get("@type") != "http://id.loc.gov/ontologies/bibframe/Issn":
         errors["parse_issn"].append(
@@ -421,34 +359,24 @@ def parse_issn(issn: str, item: dict) -> Union[dict, tuple]:
     if issn != parsed_issn:
         errors["parse_issn"].append(f"ISSN is not valid! {parsed_issn}")
         raise ValueError
-    return (data, errors, warnings)
+    return (data, errors)
 
 
 def parse_issnL(item: dict) -> Union[dict, tuple]:
     errors = {"parse_issnL": []}
-    warnings = {"parse_issnL": []}
-
     data = parse_item(item)
-
-    if "@type" not in item:
-        warnings["parse_issnL"].append(f"attribute '@type' doesnt exist in {item}")
 
     if item.get("@type") != "http://id.loc.gov/ontologies/bibframe/IssnL":
         errors["parse_issnL"].append(
             f"attribute '@type' != 'http://id.loc.gov/ontologies/bibframe/IssnL', {item}"
         )
-    return (data, errors, warnings)
+    return (data, errors)
 
 
 def parse_record(item: dict) -> Union[dict, tuple]:
     errors = {"parse_record": []}
-    warnings = {"parse_record": []}
-
     data = parse_item(item)
     record = {}
-
-    if "@type" not in item:
-        warnings["parse_record"].append(f"attribute '@type' doesnt exist in {item}")
 
     if item.get("@type") != "http://schema.org/CreativeWork":
         errors["parse_record"].append(
@@ -463,10 +391,10 @@ def parse_record(item: dict) -> Union[dict, tuple]:
             del data[key]
 
     if len(data) == 1:
-        return (data, errors, warnings)
+        return (data, errors)
     else:
         record["Record"] = data
-        return (record, errors, warnings)
+        return (record, errors)
 
 
 def parse_item(item: dict) -> Union[dict, str]:
